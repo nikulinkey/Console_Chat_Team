616106d10b32694ce75d22feee7d08021b36aef1
nikulinkey <nikulinkey@gmail.com>
2023-04-29 17:39:53 +0500

    Добавьте файлы проекта.

---
+++ Chat.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <vector>
+#include "Message.h"
+
+class Chat
+{
+	std::vector<Message> messages_;
+	bool chatWorks_ = false;
+
+	void chatMenu();
+	void sendMessage();
+	void showMessages();
+	std::vector<Message>& getMessages() { return messages_; }
+public:
+	void chatStart();
+	bool chatWorks() { return chatWorks_; }
+	void chatShutdown() { chatWorks_ = false; }
+};
